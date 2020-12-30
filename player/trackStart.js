6a794bbd138984cbf5df24b6bcc9f4343b76b91a
Leonard Scheidemantel <leonard.scheidemantel@googlemail.com>
2020-12-30 18:47:57 +0100

    Reformat now playing message

--- player/trackStart.js
+++ player/trackStart.js
@@ -1,4 +1,4 @@
 module.exports = (client, message, track) => {
     if (client.player.getQueue(message).previousTracks.length === 0 && client.player.getQueue(message).tracks.length <= 1)
-        message.channel.send(`${client.emotes.music} - Now playing ${track.title} into ${message.member.voice.channel.name} ...`);
+        message.channel.send(`${client.emotes.music} - Now playing ${track.title}`);
 };
