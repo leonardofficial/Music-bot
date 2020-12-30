4244fc9fe1f6bb535753fe45aed5ed9f9d678f78
Leonard Scheidemantel <leonard.scheidemantel@googlemail.com>
2020-12-30 18:32:31 +0100

    Add more queue details to message

--- player/trackAdd.js
+++ player/trackAdd.js
@@ -1,3 +1,7 @@
 module.exports = (client, message, queue, track) => {
-    message.channel.send(`${client.emotes.music} - ${track.title} has been added to the queue !`);
+    if (client.player.getQueue(message).tracks.length === 2) {
+        message.channel.send(`${client.emotes.music} - ${track.title} has been added to the queue !` + " There is **1 song** before you.");
+    } else {
+        message.channel.send(`${client.emotes.music} - ${track.title} has been added to the queue !` + " There are **" + (client.player.getQueue(message).tracks.length - 1) + " songs** before you.");
+    }
 };
